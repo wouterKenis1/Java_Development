00ef91651c1c0bb5bfb7476bb24f4681c320dde9
wouterKenis1 <kenis.wouter@gmail.com>
2020-08-12 12:06:24 +0200

    2020_08_12_Collections (2)

--- 2020_08_12_Collections/src/main/java/ex1/Main.java
+++ 2020_08_12_Collections/src/main/java/ex1/Main.java
@@ -1,18 +1,27 @@
 package ex1;
 
+import java.awt.*;
 import java.awt.geom.Point2D;
-import java.util.ArrayList;
+import java.util.Random;
 
 public class Main {
+    public static Random r = new Random();
+
     public static void main(String[] args) {
         Main main = new Main();
 
+        Point2D[] points = new Point2D[10];
+        generatePoints(points,20);
+        points = sortArrayPoint2D(points);
+        for (Point2D point2D: points) {
+            Point point = (Point)point2D;
+            System.out.print("(" + point.x + "," + point.y + ") ");
+        }
     }
 
 
-    public Point2D[] sortArrayPoint2D(Point2D[] array){
-
-        return array;
+    public static Point2D[] sortArrayPoint2D(Point2D[] array){
+        return quickSort(array);
     }
 
     public static Point2D[] quickSort(Point2D[] array){
@@ -50,7 +59,15 @@ public class Main {
         return list;
     }
 
+    public static Point2D[] generatePoints(Point2D[] list, int bound){
+        for(int i = 0; i < list.length; i++){
+            list[i] = generatePoint(bound);
+        }
+        return list;
+    }
 
-
+    public static Point generatePoint(int bound){
+        return new Point(r.nextInt(bound),r.nextInt(bound));
+    }
 
 }
