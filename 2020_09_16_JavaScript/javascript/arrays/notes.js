d65e1efd63495e1f5d8f94c6d7f08e7e862620bc
wouterKenis1 <kenis.wouter@gmail.com>
2020-09-16 16:11:33 +0200

    end of day upload

--- 2020_09_16_JavaScript/javascript/arrays/notes.js
+++ 2020_09_16_JavaScript/javascript/arrays/notes.js
@@ -1,4 +1,4 @@
-const notes = ["Note 1", "Note 2", "Note 3"];
+let notes = ["Note 1", "Note 2", "Note 3"];
 
 console.log(notes);
 console.log(notes.length);
@@ -8,4 +8,24 @@ console.log(notes[notes.length - 1]);
 console.log(notes.pop());
 notes.push("A New Note");
 console.log(notes);
+
+console.log(notes.shift());
+console.log(notes);
+
+console.log("----- splice -----");
+notes = ["Note 1", "Note 2", "Note 3"];
+notes.splice(1,0,"New Note");
+console.log(notes);
+
+notes = ["Note 1", "Note 2", "Note 3"];
+notes.splice(1,1,"New Note");
+console.log(notes);
+
+console.log("----- findIndex -----");
+notes = ["Note 1", "Note 2", "Note 3", "Note 4"];
+let index = notes.findIndex(function(note,index){
+    return note === "Note 3";
+    }
+)
+console.log(index);
 
--- 2020_09_16_JavaScript/javascript/arrays/todo.js
+++ 2020_09_16_JavaScript/javascript/arrays/todo.js
@@ -18,4 +18,79 @@ let printTodo = function(todo){
 printTodoAmount(todos);
 printTodos(todos);
 
+// challenge
+// remove 3rd item
+// add new item at back
+// remove first item
+console.log("-----")
+todos = ["order cat food","clean the kitchen", "buy food", "do work", "exercise"];
+todos.splice(2,1);
+todos.push("new Todo");
+todos.shift();
+console.log(todos);
+
+console.log("-----");
+todos = ["order cat food","clean the kitchen", "buy food", "do work", "exercise"];
+
+todos.forEach(
+    function(todo,index){
+        const num = index + 1;
+        console.log(`${num}. ${todo}`);
+    }
+)
+
+console.log("-----");
+for(let i=0; i<=10;i++){
+    console.log(i);
+}
+
+
+console.log("-----");
+todos = ["order cat food","clean the kitchen", "buy food", "do work", "exercise"];
+for(let i=0;i < todos.length; i++){
+    console.log(`${i+1}. ${todos[i]}`);
+}
+
+console.log("----- challenge -----");
+todos = ["order cat food","clean the kitchen", "buy food", "do work", "exercise"];
+// 1. convert array to array of objects -> text, completed
+// 2. Create function to remove todo by text value
+
+todos = [
+    {
+        text: "order cat food",
+        completed: false,
+    },
+    {
+        text: "clean the kitchen",
+        completed: false,
+    },
+    {
+        text: "buy food",
+        completed: false,
+    },
+    {
+        text:  "do work",
+        completed: false,
+    },
+    {
+        text:  "exercise",
+        completed: false,
+    },
+]
+
+let deleteTodo = function(array,todoString){
+    let index = array.findIndex(function(todo){
+        return todo.text.toLowerCase() == todoString.toLowerCase();
+    });
+    console.log(index);
+    array.splice(index,1);
+}
+
+console.log(todos);
+deleteTodo(todos, "buy food");
+console.log(todos);
+
+
+
 
